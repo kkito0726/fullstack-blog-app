e0e9797e9036b3d34113469662835b504552f811
kkito0726 <k.kito0726@gmail.com>
2023-11-06 01:17:19 +0900

    API作成

---
+++ app/api/blog/route.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const main = async () => {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("DB接続に失敗しました");
+  }
+};
+
+// 全記事取得用API
+export const GET = async (req: Request, res: NextRequest) => {
+  try {
+    await main();
+    const posts = await prisma.post.findMany();
+
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// ブログ投稿用API
+export const POST = async (req: Request, res: NextRequest) => {
+  try {
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.create({ data: { title, description } });
+
+    return NextResponse.json({ message: "Success", post }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
